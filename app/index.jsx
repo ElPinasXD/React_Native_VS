83389c216b9a06455929863a79105c2e9231f2ac
ElPinasXD <160042562+ElPinasXD@users.noreply.github.com>
2024-08-20 17:24:54 -0500

    Llege hasta el minuto 1:02:14

--- app/index.jsx
+++ app/index.jsx
@@ -1,26 +1,52 @@
-import { StyleSheet , Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Me toca importar para que me funcione
+import { ScrollView } from "react-native-gesture-handler";
+import { images } from "@/constants";
 
 // Componente App
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-    <Text className="text-3xl text-black font-pblack">Estoy en INDEX</Text>
-    <StatusBar style='auto' />
-    <Link href="/home" className="text-green-500 mt-5 text-2xl">Home</Link>
-    <Link href="/Register" className="text-red-500 mt-5 text-2xl">Registro</Link>
-  </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image 
+            source={images.logo}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode='contain'
+          />
+
+          <Image 
+          source={images.cards}
+          className="max-w-[380px] w-full h-[300px]"
+          resizeMode='contain'
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">Descubre infinitas posibilidades con{''}
+              <Text className="text-secondary-200"> Aora </Text>
+            </Text>
+
+            <Image 
+                source={images.path}
+                className="w-[136px] h-[15px] absolute 
+                -bottom-2 -right-8"
+                resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+          Donde la creatividad se encuentra con la innovación:
+          Embárcate en un viaje de exploración ilimitada con
+          </Text>
+
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
-
-/*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
-});*/
-
